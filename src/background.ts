bfe340bf60757b8c70225f244d644a8b11056e0e
l.siepsiak <l.siepsiak@medidok.de>
2024-07-03 13:45:10 +0200

    update to v5

--- src/background.ts
+++ src/background.ts
@@ -1,17 +1,17 @@
-chrome.runtime.onInstalled.addListener((details) => {
-    if (details.reason === chrome.runtime.OnInstalledReason.UPDATE) {
-        console.log(`Updated from version ${details.previousVersion} to ${chrome.runtime.getManifest().version}`);
-        // Optionally notify the user about the update
-        chrome.notifications.create({
-            type: 'basic',
-            iconUrl: 'icons/icon128.png',
-            title: 'Extension Updated. Reloading extensions',
-            message: `Updated to version ${chrome.runtime.getManifest().version}`,
-        });
+// chrome.runtime.onInstalled.addListener((details) => {
+//     if (details.reason === chrome.runtime.OnInstalledReason.UPDATE) {
+//         console.log(`Updated from version ${details.previousVersion} to ${chrome.runtime.getManifest().version}`);
+//         // Optionally notify the user about the update
+//         chrome.notifications.create({
+//             type: 'basic',
+//             iconUrl: 'icons/icon128.png',
+//             title: 'Extension Updated. Reloading extensions',
+//             message: `Updated to version ${chrome.runtime.getManifest().version}`,
+//         });
 
-        chrome.runtime.reload();
-    }
-});
+//         //chrome.runtime.reload();
+//     }
+// });
 
 chrome.runtime.onStartup.addListener(() => {
     // This will run each time Chrome starts
